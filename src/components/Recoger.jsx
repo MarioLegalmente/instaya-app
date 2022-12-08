39d4a77ace6b4be8ad942bde726d29bcf365afd0
Mario Fernandez S <mefernandez@unimagdalena.edu.co>
2022-12-07 20:41:31 -0500

    MVP InstaYA

--- src/components/Recoger.jsx
+++ src/components/Recoger.jsx
@@ -1,6 +1,63 @@
-import React from "react";
+import React, { useState } from "react"
 
 export default function Recoger(){
+    const [error, setError] = useState("")
+    const [success, setSuccss] = useState("")
+
+
+    function handleRecoger(event) {
+        event.preventDefault()
+
+        console.log({event})
+        
+        const fecha = event.target[0].value
+        const hora = event.target[1].value
+        const ancho = event.target[2].value
+        const largo = event.target[3].value
+        const alto = event.target[4].value
+        const peso = event.target[5].value
+        const direccionRecogida = event.target[6].value
+        const ciudadRecogida = event.target[7].value
+        const nombreDestinatario = event.target[8].value
+        const idDestinatario = event.target[9].value
+        const ciudadDestinatario = event.target[10].value
+        const direccionDestinatario = event.target[11].value
+      
+        const body = {
+            fecha,
+            hora,
+            ancho,
+            largo,
+            alto,
+            peso,
+            direccionRecogida,
+            ciudadRecogida,
+            nombreRecogida:"Oliver Atom",
+            nitRecogida:"0005riente",
+            nombreDestinatario,
+            nitDestinatario:idDestinatario,
+            ciudadDestinatario,
+            direccionDestinatario,
+            idUsuario:localStorage.getItem("userId"),
+            nombreCompleto:localStorage.getItem("nameUser")
+
+        }
+
+        fetch("https://instaya-tae3.onrender.com/api/service", {method:"POST", body:JSON.stringify(body), headers:{"Content-type":"application/json"}})
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data)
+
+            if(data.error === true){
+                setError(data.message)
+                return
+            }
+            setSuccss(data.message)
+            event.target.reset()
+        })
+    }
+
+    
     return(
         <div className="container mt-5">
         <div className="card">
@@ -8,15 +65,15 @@ export default function Recoger(){
                 <h5 className="card-title m-0 py-2"><strong>Gestión de Paquetes</strong> - Registro de ordenes</h5>
             </div>
             <div className="card-body">
-                <form>
+                <form onSubmit={ handleRecoger }>
                     <div className="row">
                         <div className="mb-3 col">
-                            <label htmlFor="fechaActual" className="form-label">Fecha</label>
-                            <input type="date" className="form-control" id="fechaActual" name="fechaActual" required />
+                            <label htmlFor="fecha" className="form-label">Fecha</label>
+                            <input type="date" className="form-control" id="fecha" name="fecha" required />
                         </div>
                         <div className="mb-3 col">
-                            <label htmlFor="horaActual" className="form-label">Hora</label>
-                            <input type="time" className="form-control" id="horaActual" name="horaActual" required />
+                            <label htmlFor="hora" className="form-label">Hora</label>
+                            <input type="time" className="form-control" id="hora" name="hora" required />
                         </div>
                     </div>
                     <div className="row">
@@ -81,13 +138,14 @@ export default function Recoger(){
                                 required placeholder="Ej: Calle 11 # 30 - 20" />
                         </div>
                     </div>
-
+                    {error!== "" &&<p className="alert alert-danger">{error}</p>}
+                    {success!== "" &&<p className="alert alert-success">{success}</p>}
                     <div className="d-grid col-2 mx-auto">
+
                         <div className="mb-3" >
                          <button type="submit" className="btn btn-secondary">Crear Órden</button>
                         </div>
                     </div>
-
                 </form>
             </div>
         </div>
--- src/components/Registro.jsx
+++ src/components/Registro.jsx
@@ -1,40 +1,82 @@
-import React from "react"
-import {Link } from "wouter"
+import React, { useState } from "react"
+import { Link , useLocation } from "wouter"
 
 
-export default function Registro (){
-    return(
+export default function Registro() {
+
+    const [error, setError] = useState("")
+    const [success, setSuccss] = useState("")
+    const [location, setLocation] = useLocation("")
+
+    function handleRegister(event) {
+        event.preventDefault()
+
+        console.log({event})
+        
+        const nombreCompleto = event.target[0].value
+        const usuario = event.target[1].value
+        const password = event.target[2].value
+        const correo = event.target[3].value
+
+        const body = {
+            nombreCompleto,
+            usuario,
+            password,
+            correo
+
+        }
+
+        fetch("https://instaya-tae3.onrender.com/api/user", {method:"POST", body:JSON.stringify(body), headers:{"Content-type":"application/json"}})
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data)
+
+            if(data.error === true){
+                setError(data.message)
+                return
+            }
+            setSuccss(data.message)
+            event.target.reset()
+            setTimeout(()=>{
+                setLocation("/")
+            },2000)
+
+        })
+    }
+    return (
         <div className="container">
-        <h3 className="fw-bold text-center mt-5 text-uppercase p-3 col-sm-12 col-md-7 row justify-content-end">Registro usuarios</h3>
-        <div className="row justify-content-center align-items-center" style={{height: "500px"}}>
-            <div className="bg-light col-sm-12 col-md-6 border border-white p-5">
-                <div className="mb-3">
-                    <label htmlFor="exampleFormControlInput1" className="form-label">Nombre</label>
-                    <input type="text" className="form-control" id="exampleFormControlInput1"
-                        placeholder="Fulanito" />
+            <h3 className="fw-bold text-center mt-5 text-uppercase p-3 col-sm-12 col-md-7 row justify-content-end">Registro usuarios</h3>
+            <div className="row justify-content-center align-items-center" style={{ height: "500px" }}>
+                <div className="bg-light col-sm-12 col-md-6 border border-white p-5">
+                    <form onSubmit={handleRegister}>
+                        <div className="mb-3">
+                            <label htmlFor="nombreCompleto" className="form-label">Nombre</label>
+                            <input type="text" className="form-control" id="nombreCompleto"
+                                placeholder="Fulanito" />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="usuario" className="form-label">Usurio</label>
+                            <input type="text" className="form-control" id="usuario"
+                                placeholder="InstaYa" />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="password" className="form-label">Contraseña</label>
+                            <input type="text" className="form-control" id="password"
+                                placeholder="********" />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="correo" className="form-label">Correo</label>
+                            <input type="email" className="form-control" id="correo"
+                                placeholder="team@instaya.com" />
+                        </div>
+                        {error!== "" &&<p className="alert alert-danger">{error}</p>}
+                        {success!== "" &&<p className="alert alert-success">{success}</p>}
+                        <div className="mb-3" >
+                        <button type="submit" className="btn btn-secondary">Crear</button>
+                        </div>
+                    </form>
                 </div>
-                <div className="mb-3">
-                    <label htmlFor="exampleFormControlInput1" className="form-label">Usurio</label>
-                    <input type="text" className="form-control" id="exampleFormControlInput1"
-                        placeholder="InstaYa" />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="exampleFormControlInput1" className="form-label">Contraseña</label>
-                    <input type="text" className="form-control" id="exampleFormControlInput1"
-                        placeholder="********" />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="exampleFormControlInput1" className="form-label">Correo</label>
-                    <input type="email" className="form-control" id="exampleFormControlInput1"
-                        placeholder="team@instaya.com" />
-                </div>
-
-                <div className="mb-3" >
-                  <Link href="/listado"><button type="button"className="btn btn-secondary">Crear</button></Link> 
-                </div>
-            
             </div>
         </div>
-    </div>
     )
 }
